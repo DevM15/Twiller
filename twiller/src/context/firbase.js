60c688918ded7e1ed92676843d69407742e3c23d
DevM15 <devmistry1501@gmail.com>
2025-06-10 13:22:47 +0530

    first commit

---
+++ twiller/src/context/firbase.js
@@ -0,0 +1,18 @@
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDIA7mf38QbF4VjY6k_0WxRzTR6tCNzKZo",
+  authDomain: "twitter-5a751.firebaseapp.com",
+  projectId: "twitter-5a751",
+  storageBucket: "twitter-5a751.firebasestorage.app",
+  messagingSenderId: "1042209663398",
+  appId: "1:1042209663398:web:f0302888a8d03a7434c767",
+  measurementId: "G-MVCJJ02XYK"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
+export default app
+// const analytics = getAnalytics(app);
